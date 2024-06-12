b233163cd1c8872b084707a163b04ab3a5fd2f57
Oleh K <olegk0409@gmail.com>
2024-06-12 12:55:16 +0300

    make build

---
+++ index.1980c8bd.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall");t.style.left="".concat(e.clientWidth/2-t.clientWidth/2,"px"),t.style.top="".concat(e.clientHeight/2-t.clientHeight/2,"px"),t.style.transition="none";
+//# sourceMappingURL=index.1980c8bd.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
+spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+spider.style.transition = "none";
+
+//# sourceMappingURL=index.f75de5e1.js.map
